e4b797d8690eb10f1b050cb4926d0ac8a6a1dce3
Dan Murphy <danhatesnumbers@gmail.com>
2018-12-23 19:27:25 +0000

    Added network parsing

--- src/main.rs
+++ src/main.rs
@@ -111,6 +111,38 @@ fn parse_scenario<'a>(
                 .ok_or("Could not read name of system as a string")?
                 .into();
 
+            let mut system_networks: Result<Vec<&Network>, std::boxed::Box<std::error::Error>> =
+                system_toml
+                    .get("networks")
+                    .ok_or(format!(
+                        "Could not read networks for system: {}",
+                        system.name
+                    ))?
+                    .as_array()
+                    .ok_or(format!(
+                        "Could not read networks for system: {}",
+                        system.name
+                    ))?
+                    .into_iter()
+                    .map(|network_name_toml| {
+                        let network_name = network_name_toml.as_str().ok_or(format!(
+                            "Could not parse networks for system: {}",
+                            system.name
+                        ))?;
+
+                        Ok(scenario
+                            .networks
+                            .iter()
+                            .find(|&network| network.name == network_name)
+                            .ok_or(format!(
+                                r#"System "{}" is configured to use network "{}" but no network with that name could be found"#,
+                                system.name, network_name 
+                            ))?)
+                    })
+                    .collect();
+
+            system.networks.append(&mut system_networks?);
+
             system.base_box = system_toml
                 .get("base_box")
                 .ok_or(format!(
@@ -597,7 +629,7 @@ mod tests {
         assert_eq!(scenario.systems[0].base_box, "Windows 10");
 
         assert_eq!(scenario.systems[1].name, "Server");
-        assert_eq!(scenario.systems[1].networks[1], &scenario.networks[0]);
+        assert_eq!(scenario.systems[1].networks[0], &scenario.networks[0]);
         assert_eq!(scenario.systems[1].base_box, "Debian");
 
         Ok(())
