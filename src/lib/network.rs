bc4ee14d629fb53634af0f4df1f8f57c95b0c130
Dan Murphy <danhatesnumbers@gmail.com>
2018-12-23 22:37:22 +0000

    Moved logic for parsing a network from a TOML Value to a function belonging to Network

--- src/lib/network.rs
+++ src/lib/network.rs
@@ -1,4 +1,5 @@
 use ipnet::Ipv4Net;
+use toml::Value;
 
 #[derive(Debug, PartialEq)]
 pub struct Network {
@@ -11,4 +12,62 @@ pub struct Network {
 pub enum NetworkType {
     Public,
     Internal,
+}
+
+impl Network {
+    pub fn from_toml(network_toml: &Value) -> Result<Network, std::boxed::Box<std::error::Error>> {
+        let network_name = network_toml
+            .get("name")
+            .ok_or("Could not read network name")?
+            .as_str()
+            .ok_or("Could not read network name as a string")?
+            .into();
+
+        let network_type_unparsed = network_toml
+            .get("type")
+            .ok_or(format!(
+                "Could not read network type for network: {}",
+                network_name
+            ))?
+            .as_str()
+            .ok_or(format!(
+                "Could not read network type as a string for network: {}",
+                network_name
+            ))?
+            .into();
+
+        let network_type = match network_type_unparsed {
+            "Public" => Ok(NetworkType::Public),
+            "Internal" => Ok(NetworkType::Internal),
+            _ => Err(format!(
+                "Could not parse network type as a valid type for network: {}. Valid types are: Public, Internal",
+                network_name
+            )),
+        };
+
+        let subnet: Ipv4Net = network_toml
+            .get("subnet")
+            .ok_or(format!(
+                "Could not read subnet for network: {}",
+                network_name
+            ))?
+            .as_str()
+            .ok_or(format!(
+                "Could not read subnet as string for network: {}",
+                network_name
+            ))?
+            .parse()
+            .map_err(|_| {
+                format!(
+                    "Could not parse subnet as a valid CIDR range for network: {}",
+                    network_name
+                )
+            })?;
+
+        Ok(Network {
+            name: network_name,
+            network_type: network_type?,
+            subnet: subnet,
+        })
+    }
 }
--- src/lib/scenario.rs
+++ src/lib/scenario.rs
@@ -1,6 +1,5 @@
-use crate::lib::network::{Network, NetworkType};
+use crate::lib::network::Network;
 use crate::lib::system::System;
-use ipnet::Ipv4Net;
 use toml::Value;
 
 #[allow(unused_imports)]
@@ -38,56 +37,7 @@ impl<'a> Scenario<'a> {
             .as_array()
             .ok_or("Could not read networks from configuration")?
             .into_iter()
-            .map(|network_toml| {
-                let network_name = network_toml
-                    .get("name")
-                    .ok_or("Could not read network name")?
-                    .as_str()
-                    .ok_or("Could not read network name as a string")?
-                    .into();
-
-                let network_type_unparsed = network_toml
-                    .get("type")
-                    .ok_or(format!(
-                        "Could not read network type for network: {}",
-                        network_name
-                    ))?
-                    .as_str()
-                    .ok_or(format!(
-                        "Could not read network type as a string for network: {}",
-                        network_name
-                    ))?
-                    .into();
-
-                let network_type = match network_type_unparsed {
-                    "Public" => Ok(NetworkType::Public),
-                    "Internal" => Ok(NetworkType::Internal),
-                    _ => Err(format!(
-                        "Could not parse network type as a valid type for network: {}. Valid types are: Public, Internal",
-                        network_name
-                    )),
-                };
-
-                let subnet: Ipv4Net = network_toml
-                    .get("subnet")
-                    .ok_or(format!(
-                        "Could not read subnet for network: {}",
-                        network_name))?
-                    .as_str()
-                    .ok_or(format!(
-                        "Could not read subnet as string for network: {}",
-                        network_name))?
-                    .parse()
-                    .map_err(|_| format!(
-                        "Could not parse subnet as a valid CIDR range for network: {}",
-                        network_name))?;
-
-                Ok(Network {
-                    name: network_name,
-                    network_type: network_type?,
-                    subnet: subnet,
-                })
-            })
+            .map(Network::from_toml)
             .collect();
 
         scenario.networks.append(&mut networks?);
