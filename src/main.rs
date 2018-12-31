590187c9aa5d85c511e56de689eef9ae7ff6453b
Dan Murphy <danhatesnumbers@gmail.com>
2018-12-31 14:27:02 +0000

    Binary learned to build a vagrantfile from a scenario

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ fn main() -> Result<(), std::boxed::Box<std::error::Error>> {
         .author("Daniel Murphy <danhatesnumbers@gmail.com>")
         .subcommand(
             SubCommand::with_name("plan")
-                .about("plan Vagrantfile from Scenario")
+                .about("plan output from Scenario")
                 .arg(
                     Arg::with_name("scenario")
                         .short("s")
@@ -26,13 +26,49 @@ fn main() -> Result<(), std::boxed::Box<std::error::Error>> {
                         .help("path to Scenario to plan in TOML format"),
                 ),
         )
+        .subcommand(
+            SubCommand::with_name("vagrantfile")
+                .about("build vagrantfile from Scenario")
+                .arg(
+                    Arg::with_name("scenario")
+                        .short("s")
+                        .required(true)
+                        .takes_value(true)
+                        .value_name("SCENARIO_PATH")
+                        .help("path to Scenario to build in TOML format"),
+                )
+                .arg(
+                    Arg::with_name("output")
+                        .short("o")
+                        .required(true)
+                        .takes_value(true)
+                        .value_name("OUTPUT_PATH")
+                        .help("output path for vagrantfile"),
+                ),
+        )
         .get_matches();
 
     if let Some(plan) = arg_matches.subcommand_matches("plan") {
         let scenario_path = Path::new(plan.value_of("scenario").unwrap());
 
         let scenario_toml = fs::read_to_string(scenario_path)?.parse::<Value>()?;
-        println!("{:?}", Scenario::from_toml(&scenario_toml));
+        println!("{:?}", Scenario::from_toml(&scenario_toml)?);
+    };
+
+    if let Some(vagrantfile) = arg_matches.subcommand_matches("vagrantfile") {
+        let scenario_path = Path::new(vagrantfile.value_of("scenario").unwrap());
+
+        let scenario_toml = fs::read_to_string(scenario_path)?.parse::<Value>()?;
+
+        let mut scenario = Scenario::from_toml(&scenario_toml)?;
+        for system in scenario.systems.iter_mut() {
+            system.configure_networking(&scenario.networks)?;
+        }
+
+        let output = scenario.to_vagrantfile()?;
+
+        let vagrantfile_path = Path::new(vagrantfile.value_of("output").unwrap());
+        fs::write(vagrantfile_path, output)?;
     };
 
     Ok(())
